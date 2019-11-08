7bc2f7e19c13df359d2c35d49a2c62c196224962
Allen <502624709@qq.com>
2019-11-08 11:23:55 +0800

    重构代码实现

--- src/main/java/com/github/zhangquanli/aliyun/sms/AliyunSms.java
+++ src/main/java/com/github/zhangquanli/aliyun/sms/AliyunSms.java
@@ -14,88 +14,88 @@ public interface AliyunSms {
     /**
      * 短信发送接口，支持在一次请求中向多个不同的手机号码发送同样内容的短信
      *
-     * @param request 请求参数
+     * @param sendSmsRequest 请求参数
      * @return 响应结果
      */
-    SendSmsResponse sendSms(SendSmsRequest request);
+    SendSmsResponse sendSms(SendSmsRequest sendSmsRequest);
 
     /**
      * 短信批量发送接口，支持在一次请求中分别向多个不同的手机号码发送不同签名的短信
      *
-     * @param request 请求参数
+     * @param sendBatchSmsRequest 请求参数
      * @return 响应结果
      */
-    SendBatchSmsResponse sendBatchSms(SendBatchSmsRequest request);
+    SendBatchSmsResponse sendBatchSms(SendBatchSmsRequest sendBatchSmsRequest);
 
     /**
      * 查询发送记录
      *
-     * @param request 请求参数
+     * @param querySendDetailsRequest 请求参数
      * @return 响应结果
      */
-    QuerySendDetailsResponse querySendDetails(QuerySendDetailsRequest request);
+    QuerySendDetailsResponse querySendDetails(QuerySendDetailsRequest querySendDetailsRequest);
 
     /**
      * 申请短信签名
      *
-     * @param request 请求参数
+     * @param addSmsSignRequest 请求参数
      * @return 响应结果
      */
-    AddSmsSignResponse addSmsSign(AddSmsSignRequest request);
+    AddSmsSignResponse addSmsSign(AddSmsSignRequest addSmsSignRequest);
 
     /**
      * 删除短信签名
      *
-     * @param request 请求参数
+     * @param deleteSmsSignRequest 请求参数
      * @return 响应结果
      */
-    DeleteSmsSignResponse deleteSmsSign(DeleteSmsSignRequest request);
+    DeleteSmsSignResponse deleteSmsSign(DeleteSmsSignRequest deleteSmsSignRequest);
 
     /**
      * 修改未审核通过的短信签名，并重新提交审核
      *
-     * @param request 请求参数
+     * @param modifySmsSignRequest 请求参数
      * @return 响应结果
      */
-    ModifySmsSignResponse modifySmsSign(ModifySmsSignRequest request);
+    ModifySmsSignResponse modifySmsSign(ModifySmsSignRequest modifySmsSignRequest);
 
     /**
      * 查询短信签名的审批状态
      *
-     * @param request 请求参数
+     * @param querySmsSignRequest 请求参数
      * @return 响应结果
      */
-    QuerySmsSignResponse querySmsSign(QuerySmsSignRequest request);
+    QuerySmsSignResponse querySmsSign(QuerySmsSignRequest querySmsSignRequest);
 
     /**
      * 申请短信模板
      *
-     * @param request 请求参数
+     * @param addSmsTemplateRequest 请求参数
      * @return 响应结果
      */
-    AddSmsTemplateResponse addSmsTemplate(AddSmsTemplateRequest request);
+    AddSmsTemplateResponse addSmsTemplate(AddSmsTemplateRequest addSmsTemplateRequest);
 
     /**
      * 删除短信模板
      *
-     * @param request 请求参数
+     * @param deleteSmsTemplateRequest 请求参数
      * @return 响应结果
      */
-    DeleteSmsTemplateResponse deleteSmsTemplate(DeleteSmsTemplateRequest request);
+    DeleteSmsTemplateResponse deleteSmsTemplate(DeleteSmsTemplateRequest deleteSmsTemplateRequest);
 
     /**
      * 修改未审核通过的短信模板，并重新提交审核
      *
-     * @param request 请求参数
+     * @param modifySmsTemplateRequest 请求参数
      * @return 响应结果
      */
-    ModifySmsTemplateResponse modifySmsTemplate(ModifySmsTemplateRequest request);
+    ModifySmsTemplateResponse modifySmsTemplate(ModifySmsTemplateRequest modifySmsTemplateRequest);
 
     /**
      * 查询短信模板的审核状态
      *
-     * @param request 请求参数
+     * @param querySmsTemplateRequest 请求参数
      * @return 响应结果
      */
-    QuerySmsTemplateResponse querySmsTemplate(QuerySmsTemplateRequest request);
+    QuerySmsTemplateResponse querySmsTemplate(QuerySmsTemplateRequest querySmsTemplateRequest);
 }
